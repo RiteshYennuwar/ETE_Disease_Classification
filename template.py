849029bce804ff5ceebd678ac39f4c4d8cab5e05
RiteshYennuwar <yennuwarritesh@hotmail.com>
2024-05-26 00:33:23 +0530

    generated project structure using template.py

---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+
+project_name = 'Disease_classification(CNN)'
+
+lsit_of_files = [
+    '.github/workflows/.gitkeep',
+    'src/__init__.py',
+    'src/components/__init__.py',
+    'src/utils/__init__.py',
+    'src/config/__init__.py',
+    'src/config/configuration.py',
+    'src/pipeline/__init__.py',
+    'src/entity/__init__.py',
+    'src/constants/__init__.py',
+    'config/config.ymal',
+    'dvc.ymal',
+    'prams.ymal',
+    'requirements.txt',
+    'setup.py',
+    'research/trails.ipynb'
+]
+
+for filepath in lsit_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != '':
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f'creating:{filedir} for the file {filename}')
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f'creating empty file: {filepath}')
+    else:
+        logging.info(f'{filepath} already exists')
