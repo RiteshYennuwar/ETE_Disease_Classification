478188da7d49ead2609a0b6ef817580d3d2b2de3
RiteshYennuwar <yennuwarritesh@hotmail.com>
2024-05-26 17:12:59 +0530

    added some common util functions

---
+++ src/utils/common.py
@@ -0,0 +1,37 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from src import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f'yaml file: {path_to_yaml} loaded successfully')
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError('yaml file is empty')
+    except Exception as e:
+        raise e
+    
+@ensure_annotations
+def create_dir(path_to_dir:list, verbose = True):
+    for path in path_to_dir:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f'created a directory at: {path}')
+
+@ensure_annotations
+def save_json(path:Path, data: dict):
+    with open(path,'w') as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f'json file saved at {path}')
