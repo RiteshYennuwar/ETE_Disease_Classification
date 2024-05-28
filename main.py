82a297fcc33fd9a3853794481a606d74345255e2
RiteshYennuwar <yennuwarritesh@hotmail.com>
2024-05-28 23:42:18 +0530

    model training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src import logger
 from src.pipeline.STAGE1_data_ingestion import DataIngestionPipeline
 from src.pipeline.STAGE2_base_model import BaseModelPipeline
+from src.pipeline.STAGE3_training import ModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 
@@ -19,6 +20,17 @@ try:
     logger.info(f'---->> Stage {STAGE_NAME} Started <<----')
     base_model = BaseModelPipeline()
     base_model.main()
+    logger.info(f'---->> Stage {STAGE_NAME} Completed <<----\n\nx===================x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Training'
+
+try:
+    logger.info(f'---->> Stage {STAGE_NAME} Started <<----')
+    training = ModelTrainingPipeline()
+    training.main()
     logger.info(f'---->> Stage {STAGE_NAME} Completed <<----\n\nx===================x')
 except Exception as e:
     logger.exception(e)
---
+++ src/pipeline/STAGE3_training.py
@@ -0,0 +1,34 @@
+from src.config.configuration import ConfigurationManager
+from src.components.callbacks import Callbacks
+from src.components.model_training import Training
+from src import logger
+
+STAGE_NAME = 'Training'
+
+class ModelTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        callbacks_config = config.get_callbacks_config()
+        callbacks = Callbacks(config=callbacks_config)
+        callback_list = callbacks.get_tb_ckpt_callbacks()
+
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train(
+            callbak_list=callback_list
+        )
+
+if __name__ == '__main__':
+    try:
+        logger.info(f'---->> Stage {STAGE_NAME} Started <<----')
+        training = ModelTrainingPipeline()
+        training.main()
+        logger.info(f'---->> Stage {STAGE_NAME} Completed <<----\n\nx===================x')
+    except Exception as e:
+        logger.exception(e)
+        raise e
