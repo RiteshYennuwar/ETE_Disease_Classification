ed2930e2fd52205783c84fd586c23cda635fe2bf
RiteshYennuwar <yennuwarritesh@hotmail.com>
2024-05-29 14:58:17 +0530

    made changes to deploy in azure

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def predictRoute():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 4000)
+    # app.run(host='0.0.0.0', port = 4000)
+    app.run(host='0.0.0.0', port = 80)
